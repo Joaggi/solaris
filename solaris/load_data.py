09826c497a752e67f09e21b690e17d2ed5989f53
Joseph Gallego <joaggi@gmail.com>
2026-05-29 21:17:32 +0000

    Adding missing channel downstream task

--- solaris/load_data.py
+++ solaris/load_data.py
@@ -64,3 +64,34 @@ class CustomDataset_pretrain(Dataset):
         target = load_target_stack(self.root_dir, future_timestamp, AIA_PRETRAIN_WAVELENGTHS)
 
         return data, target, timestamp_to_datetime(current_timestamp)
+
+
+
+class CustomDataset_missing_channel(Dataset):
+    def __init__(self, root_dir, data_set="train", id_dir=None):
+        self.root_dir = root_dir
+        self.data_set = data_set
+        self.id_dir = resolve_id_dir(id_dir, data_root=root_dir)
+        self.ids = self._get_valid_ids()
+
+    def _get_valid_ids(self):
+        """Load valid data IDs from the pretraining ID file."""
+        return read_id_file(self.id_dir / f"{self.data_set}_id.txt")
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, idx):
+        """Get two 12-hour-separated history states and the 12-hour forecast target."""
+        corrupted_current_timestamp = self.ids[idx]
+        target_current_timestamp = self.ids[idx]
+
+        current_data = load_wavelength_stack(self.root_dir, corrupted_current_timestamp, AIA_INPUT_WAVELENGTHS)
+        data = torch.unsqueeze(current_data, dim=0)
+        target = load_target_stack(self.root_dir, target_current_timestamp, AIA_PRETRAIN_WAVELENGTHS)
+
+        random_missing_channel = torch.randint(len(AIA_INPUT_WAVELENGTHS), (1,)).item()
+        data[:, random_missing_channel, :, :] = 0
+
+        return data, target, timestamp_to_datetime(corrupted_current_timestamp)
+
